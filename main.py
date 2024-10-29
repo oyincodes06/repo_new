b7520cfd8b2f09611ea3a8415a14dd242342f5ec
AbiodunDev23 <124578605+AbiodunDev23@users.noreply.github.com>
2024-10-29 09:44:11 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+my_info = {
+    "First_name" : "Oyinlade_Oyinloye",
+    "Age" : 20,
+    "Skills" : ["Python", "HTML", "CSS", "NODEJS"],
+    "Country" : "Nigeria",
+    "Sex" : "Male"
+}
+
+for key, values in my_info.items():
+    print(f"My {key}: {values}")
